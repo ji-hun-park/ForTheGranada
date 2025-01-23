48be30030253c3c55eed12074027e39209f0e9d3
JiHun <pgh979@gmail.com>
2025-01-24 05:22:02 +0900

    UI 매니저 초기 설정

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // 싱글톤 패턴 적용
+    public static UIManager Instance;
+    
+    void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Instance 존재 유무에 따라 게임 매니저 파괴 여부 정함
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // 기존에 존재 안하면 이걸로 대체하고 파괴하지 않기
+        }
+        else
+        {
+            Destroy(gameObject); // 기존에 존재하면 자신파괴
+        }
     }
 }
